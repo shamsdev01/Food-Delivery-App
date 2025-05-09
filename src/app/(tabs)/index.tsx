da8b7f53b29f64d6f1829c7eb4fc29def0a1b541
Ajasa Shamsudeen <shamsudeenajasa@gmail.com>
2025-05-09 12:44:38 +0100

    firstcustomcomponent

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,44 +1,20 @@
 import { StyleSheet,Text, View,Image } from 'react-native';
-import { blue } from 'react-native-reanimated/lib/typescript/Colors';
-import Colors from '../constants/Colors';
 import products from '@/assets/data/products';
+import ProductListItem from '../components/ProductList';
 
 
 
-const product = products[1];
 
-
-export default function TabOneScreen() {
+export default function MenuScreen() {
   return (
-    <View style={styles.container}>
-      <Image source={{uri: product.image}}  style={styles.Image} />
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>${product.price}</Text>
-    </View>
+   <View>
+    <ProductListItem product={products[5]}/>
+    <ProductListItem product={products[1]}/>
+   </View>
   );
 }
  
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'white',
-    padding:10,
-    borderRadius:20,
-
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: '700', 
-    marginVertical: 10,
-    
-  },
-  price:{
-     color: Colors.light.tint,  
-     fontWeight: 'bold',
-
-  },
-  Image:{
-    width: '100%',
-    aspectRatio: 1,
-  },
+ 
 });
   
