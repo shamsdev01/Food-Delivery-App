61d3ab6786d178b6104b86c486aa70503a313e02
Ajasa Shamsudeen <shamsudeenajasa@gmail.com>
2025-05-09 02:54:00 +0100

    built

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,31 +1,44 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet,Text, View,Image } from 'react-native';
+import { blue } from 'react-native-reanimated/lib/typescript/Colors';
+import Colors from '../constants/Colors';
+import products from '@/assets/data/products';
 
-import EditScreenInfo from '@/src/app/components/EditScreenInfo';
-import { Text, View } from '@/src/app/components/Themed';
+
+
+const product = products[1];
+
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Hello World</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Image source={{uri: product.image}}  style={styles.Image} />
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
     </View>
   );
 }
-
+ 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'white',
+    padding:10,
+    borderRadius:20,
+
   },
   title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+    fontSize: 18,
+    fontWeight: '700', 
+    marginVertical: 10,
+    
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  price:{
+     color: Colors.light.tint,  
+     fontWeight: 'bold',
+
+  },
+  Image:{
+    width: '100%',
+    aspectRatio: 1,
   },
 });
+  
